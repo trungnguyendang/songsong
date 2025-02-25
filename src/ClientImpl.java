ac1b2416bd5bd5bf1f8fbad2d32a6e42913239fc
trungnd <trungnd>
2025-02-25 15:35:17 +0700

    update|client ping to others, show list of client

--- src/ClientImpl.java
+++ src/ClientImpl.java
@@ -1,16 +1,22 @@
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.*;
 
 public class ClientImpl extends UnicastRemoteObject implements ClientInterface {
-    // Constructor bắt buộc phải có để xử lý RemoteException
-    protected ClientImpl() throws RemoteException {
-        super();
+
+    private Map<String, String> clientRegistry = new HashMap<>();
+
+    public ClientImpl() throws RemoteException {}
+
+
+    @Override
+    public void registerClient(String clientName, String ip) throws RemoteException {
+        clientRegistry.put(clientName, ip);
+        System.out.println("Client registered: " + clientName + " - " + ip);
     }
 
-    // Thực thi phương thức addNumber
     @Override
-    public int addNumber(int x, int y) throws RemoteException {
-        System.out.println("Client request to add 2 numbers: " + x + " + " + y);
-        return x + y;
+    public List<String> getClientList() throws RemoteException {
+        return new ArrayList<>(clientRegistry.keySet());
     }
 }
--- src/ClientInterface.java
+++ src/ClientInterface.java
@@ -1,6 +1,8 @@
 import java.rmi.*;
+import java.util.List;
 
 public interface ClientInterface extends Remote {
-    public int addNumber(int x, int y) throws RemoteException;
-
+//    public int addNumber(int x, int y) throws RemoteException;
+    void registerClient(String clientName, String ip) throws RemoteException;
+    List<String> getClientList() throws RemoteException;
 }
